f7c8c6b9c5f9ffd02715aba0c8e66a10e51e1aac
DuongNManh <manhduonglhp4@gmail.com>
2024-09-13 23:58:11 +0700

    fix: refactor component

--- src/App.jsx
+++ src/App.jsx
@@ -10,63 +10,34 @@ import Register from "./pages/register/Register";
 import "./styles/style.scss";
 import { Helmet } from "react-helmet";
 import { ThemeProvider } from "./pages/theme/ThemeContext";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./AuthContext";
 import KoiDetail from "./pages/koiDetail/KoiDetail";
 import koi_data from "./utils/data/koi_data.json";
-import { useState } from "react";
-import { fetchGoogleClientId } from "./utils/apiUtils.js";
 
 const TITLE = "Auction Koi";
 
 function App() {
-  const [googleClientId, setGoogleClientId] = useState("");
-
-  useEffect(() => {
-    const loadGoogleClientId = async () => {
-      const clientId = await fetchGoogleClientId();
-      if (clientId) {
-        setGoogleClientId(clientId);
-      }
-    };
-    loadGoogleClientId();
-  }, []);
-
-  if (!googleClientId) {
-    return <div>Loading...</div>; // Or some other loading indicator
-  }
-
   return (
-    <GoogleOAuthProvider
-      clientId={googleClientId}
-      onScriptLoadError={() =>
-        console.log("Failed to load Google Sign-In script")
-      }
-      onScriptLoadSuccess={() =>
-        console.log("Google Sign-In script loaded successfully")
-      }
-    >
-      <AuthProvider>
-        <ThemeProvider>
-          <Helmet>
-            <title>{TITLE}</title>
-          </Helmet>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auctions" element={<Auctions />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/koi/:id"
-              element={<KoiDetail koiData={koi_data.items} />}
-            />
-          </Routes>
-          <Footer />
-        </ThemeProvider>
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <Helmet>
+          <title>{TITLE}</title>
+        </Helmet>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auctions" element={<Auctions />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/koi/:id"
+            element={<KoiDetail koiData={koi_data.items} />}
+          />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 

c5c4d23869d074550c6f5c5192a6c0cdcca6341e
lcaohoanq <hoangclw@gmail.com>
2024-09-22 20:34:42 +0700

    feat: deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: '/Koi-Auction-FE/', // Replace with your repository name
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",

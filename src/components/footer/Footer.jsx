d5f3a246eb0fc893113b7ac3fe805e7944a5fe3b
lcaohoanq <hoangclw@gmail.com>
2024-09-24 17:37:15 +0700

    style: re-style HomePage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Footer.scss";
 import SecurityIcon from "@mui/icons-material/Security";
 import DescriptionIcon from "@mui/icons-material/Description";
 import HomeIcon from "@mui/icons-material/Home";
@@ -7,81 +6,77 @@ import AuctionIcon from "@mui/icons-material/Gavel";
 import InfoIcon from "@mui/icons-material/Info";
 import LoginIcon from "@mui/icons-material/Login";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-content">
-        <div className="footer-section">
-          <h3 className="footer-title">Navigation</h3>
-          <a href="/" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <HomeIcon />
-              </div>
-            </div>
-            <span>Home</span>
-          </a>
-          <a href="/auctions" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <AuctionIcon />
-              </div>
-            </div>
-            <span>Auctions</span>
-          </a>
-          <a href="/about" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <InfoIcon />
-              </div>
-            </div>
-            <span>About</span>
-          </a>
+    <footer
+      className="shadow-lg border-solid border-t-4 bg-[#F1F1F1] dark:bg-gray-800 text-white p-8"
+      style={{ boxShadow: "0 -2px 2px rgba(0, 0, 0, 0.1)" }}
+    >
+      <div className="max-w-6xl mx-auto flex justify-start flex-wrap gap-12">
+        <Link to="/" className="mb-4 md:mb-0">
+          <img
+            src="/public/koi-svgrepo-com.svg"
+            alt="Koi Auction Logo"
+            className="w-12 h-auto max-w-full"
+          />
+        </Link>
+        <div className="flex flex-col mb-4">
+          <h3 className="text-[#121212] text-xl mb-4 font-bold bg-gray-300 rounded-xl p-2">
+            Navigation
+          </h3>
+          <FooterLink href="/" icon={<HomeIcon />} text="Home" />
+          <FooterLink href="/auctions" icon={<AuctionIcon />} text="Auctions" />
+          <FooterLink href="/about" icon={<InfoIcon />} text="About" />
         </div>
-        <div className="footer-section">
-          <h3 className="footer-title">Policy</h3>
-          <a href="/privacy" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <SecurityIcon />
-              </div>
-            </div>
-            <span>Privacy Policy</span>
-          </a>
-          <a href="/terms" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <DescriptionIcon />
-              </div>
-            </div>
-            <span>Terms and Conditions</span>
-          </a>
+        <div className="flex flex-col mb-4">
+          <h3 className="text-[#121212] text-xl mb-4 font-bold bg-gray-300 rounded-xl p-2">
+            Policy
+          </h3>
+          <FooterLink
+            href="/privacy"
+            icon={<SecurityIcon />}
+            text="Privacy Policy"
+          />
+          <FooterLink
+            href="/terms"
+            icon={<DescriptionIcon />}
+            text="Terms and Conditions"
+          />
         </div>
-        <div className="footer-section">
-          <h3 className="footer-title">Account</h3>
-          <a href="/login" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <LoginIcon />
-              </div>
-            </div>
-            <span>Login</span>
-          </a>
-          <a href="/register" className="footer-link">
-            <div className="svg-wrapper-1">
-              <div className="svg-wrapper">
-                <PersonAddIcon />
-              </div>
-            </div>
-            <span>Register</span>
-          </a>
+        <div className="flex flex-col mb-4">
+          <h3 className="text-[#121212] text-xl mb-4 font-bold bg-gray-300 rounded-xl p-2">
+            Account
+          </h3>
+          <FooterLink href="/login" icon={<LoginIcon />} text="Login" />
+          <FooterLink
+            href="/register"
+            icon={<PersonAddIcon />}
+            text="Register"
+          />
         </div>
       </div>
-      <div className="footer-bottom">
+      <div className="text-center mt-8 text-sm text-gray-400">
         AuctionKoi.com is a division of SelectKoi.com
       </div>
     </footer>
+  );
+}
+
+function FooterLink({ href, icon, text }) {
+  return (
+    <a
+      href={href}
+      className="flex items-center text-[#121212] px-4 py-2 rounded-full mb-2 transition duration-300 ease-in-out hover:bg-gray-700 dark:hover:bg-gray-600 group"
+    >
+      <div className="transform transition-transform duration-300 ease-in-out group-hover:translate-x-1 group-hover:rotate-12">
+        {icon}
+      </div>
+      <span className="ml-2 transition-all duration-300 ease-in-out group-hover:opacity-0 group-hover:translate-x-8">
+        {text}
+      </span>
+    </a>
   );
 }
 

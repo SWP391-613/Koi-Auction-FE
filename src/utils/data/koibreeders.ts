300813e19240698c3b101d14565694b0e067c265
lcaohoanq <hoangclw@gmail.com>
2024-09-25 07:34:22 +0700

    chore: upload image using offline resources

--- src/utils/data/koibreeders.ts
+++ src/utils/data/koibreeders.ts
@@ -1,16 +1,33 @@
 export const koiBreeders = [
-  { name: "NND", logo: "https://auctionkoi.com/images/nnd-logo.png" },
-  { name: "Marushin", logo: "https://auctionkoi.com/images/marushin-logo.png" },
-  { name: "Sakai", logo: "https://auctionkoi.com/images/sakai-logo.png" },
-  { name: "ISA", logo: "https://auctionkoi.com/images/isa-logo.png" },
-  { name: "Maruho", logo: "https://auctionkoi.com/images/maruhiro-logo.png" },
-  { name: "Torazo", logo: "https://auctionkoi.com/images/torazo-logo.png" },
-  { name: "Shinoda", logo: "https://auctionkoi.com/images/shinoda-logo.png" },
-  { name: "Kanno", logo: "https://auctionkoi.com/images/kanno-logo.png" },
-  { name: "Dainichi", logo: "https://auctionkoi.com/images/dainichi-logo.png" },
-  { name: "Omosako", logo: "https://auctionkoi.com/images/omosako-logo.png" },
-  { name: "Izumiya", logo: "https://auctionkoi.com/images/izumiya-logo.png" },
-  { name: "Marudo", logo: "https://auctionkoi.com/images/marudo-logo.png" },
-  { name: "Marujyu", logo: "https://auctionkoi.com/images/marujyu-logo.png" },
-  { name: "Shintaro", logo: "https://auctionkoi.com/images/shintaro-logo.png" },
+  { name: "NND", logo: "/koibreeder/nnd-logo.png"},
+  { name: "Marushin", logo: "/koibreeder/marushin-logo.png" },
+  { name: "Sakai", logo: "/koibreeder/sakai-logo.png"},
+  { name: "ISA", logo: "/koibreeder/isa-logo.png"},
+  { name: "Maruho", logo: "/koibreeder/sakai-logo.png" },
+  { name: "Torazo", logo: "/koibreeder/torazo-logo.png"},
+  { name: "Shinoda", logo: "/koibreeder/shinoda-logo.png"},
+  { name: "Kanno", logo: "/koibreeder/kanno-logo.png"},
+  { name: "Dainichi", logo: "/koibreeder/dainichi-logo.png" },
+  { name: "Omosako", logo: "/koibreeder/omosako-logo.png"},
+  { name: "Izumiya", logo: "/koibreeder/izumiya-logo.png"},
+  { name: "Marudo", logo: "/koibreeder/marudo-logo.png"},
+  { name: "Marujyu", logo: "/koibreeder/marujyu-logo.png"},
+  { name: "Shintaro", logo: "/koibreeder/shintaro-logo.png" },
 ];
+
+/*
+https://auctionkoi.com/images/nnd-logo.png" },
+https://auctionkoi.com/images/marushin-logo.png
+https://auctionkoi.com/images/sakai-logo.png" }
+https://auctionkoi.com/images/isa-logo.png" },
+https://auctionkoi.com/images/maruhiro-logo.png
+https://auctionkoi.com/images/torazo-logo.png"
+https://auctionkoi.com/images/shinoda-logo.png"
+https://auctionkoi.com/images/kanno-logo.png" }
+https://auctionkoi.com/images/dainichi-logo.png
+https://auctionkoi.com/images/omosako-logo.png"
+https://auctionkoi.com/images/izumiya-logo.png"
+https://auctionkoi.com/images/marudo-logo.png"
+https://auctionkoi.com/images/marujyu-logo.png"
+https://auctionkoi.com/images/shintaro-logo.png
+*/

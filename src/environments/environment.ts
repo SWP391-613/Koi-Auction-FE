db8b90846d0b5ced38e4b131af739c19bf2834ba
lcaohoanq <hoangclw@gmail.com>
2024-10-07 11:33:49 +0700

    cd: adjust build command

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: import.meta.env.PROD, // This will be true in production and false in development
   be: {
-    baseUrl: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:4000",  // Default to localhost:4000 for development
+    baseUrl: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:4000", // Default to localhost:4000 for development
     apiPrefix: "/api/v1",
     endPoint: {
       login: "/login",

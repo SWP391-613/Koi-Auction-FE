1003d4432d2b4a433f26f0732eade99bb835850c
lcaohoanq <hoangclw@gmail.com>
2024-10-09 20:44:42 +0700

    feat: include grafa dashboard to manager

--- src/layouts/dashboard/DashBoardLayout.tsx
+++ src/layouts/dashboard/DashBoardLayout.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { Box, List, ListItem, ListItemText } from "@mui/material";
+import React, { ReactNode } from "react";
+import { Box, List, ListItem, ListItemText, ListItemButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { ROUTING_PATH } from "~/constants/endPoints";
 
-const DashboardLayout = ({ children }) => {
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <Box display="flex" height="100vh">
       <Box
@@ -10,23 +15,35 @@ const DashboardLayout = ({ children }) => {
         sx={{ width: "240px", backgroundColor: "#f4f4f4", p: 2 }}
       >
         <List>
-          <ListItem button component={Link} to="/manager/koi">
-            <ListItemText primary="Koi" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_KOI}>
+              <ListItemText primary="Koi" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button component={Link} to="/manager/auctions">
-            <ListItemText primary="Auctions" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_AUCTIONS}>
+              <ListItemText primary="Auctions" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button component={Link} to="/manager/breeder">
-            <ListItemText primary="Breeder" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_BREEDER}>
+              <ListItemText primary="Breeder" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button component={Link} to="/manager/member">
-            <ListItemText primary="Member" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_MEMBER}>
+              <ListItemText primary="Member" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button component={Link} to="/manager/staff">
-            <ListItemText primary="Staff" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_STAFF}>
+              <ListItemText primary="Staff" />
+            </ListItemButton>
           </ListItem>
-          <ListItem button component={Link} to="/manager/setting">
-            <ListItemText primary="Settings" />
+          <ListItem>
+            <ListItemButton component={Link} to={ROUTING_PATH.MANAGERS_SETTING}>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
           </ListItem>
         </List>
       </Box>

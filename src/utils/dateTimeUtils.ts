0b3ded8948a756a77285894011126aeba2123969
lcaohoanq <hoangclw@gmail.com>
2024-10-11 22:27:47 +0700

    style: re-style koi in auction

--- src/utils/dateTimeUtils.ts
+++ src/utils/dateTimeUtils.ts
@@ -1,7 +1,17 @@
+import {
+  differenceInDays,
+  isAfter,
+  isBefore,
+  isValid,
+  parseISO,
+} from "date-fns";
+
 export const getAuctionStatus = (
   startTime: string,
   endTime: string,
 ): string => {
+  console.log(`Data received: ${startTime} - ${endTime}`);
+
   const now = new Date();
 
   // Use the Date constructor to parse ISO 8601 date string
@@ -25,6 +35,49 @@ export const getAuctionStatus = (
   else {
     const diffTime = now.getTime() - end.getTime();
     const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+
+    if (diffDays < 1) {
+      return "Ended today";
+    } else if (diffDays < 30) {
+      return `Ended ${diffDays} day${diffDays > 1 ? "s" : ""} ago`;
+    } else if (diffDays < 365) {
+      const diffMonths = Math.floor(diffDays / 30);
+      return `Ended ${diffMonths} month${diffMonths > 1 ? "s" : ""} ago`;
+    } else {
+      const diffYears = Math.floor(diffDays / 365);
+      return `Ended ${diffYears} year${diffYears > 1 ? "s" : ""} ago`;
+    }
+  }
+};
+
+export const getAuctionStatusV2 = (
+  startTime: string,
+  endTime: string,
+): string => {
+  console.log(`Data received: ${startTime} - ${endTime}`);
+
+  const now = new Date();
+
+  // Parse dates using date-fns to ensure they are valid
+  const start = parseISO(startTime);
+  const end = parseISO(endTime);
+
+  // Check for invalid dates
+  if (!isValid(start) || !isValid(end)) {
+    return "Invalid date"; // Handle invalid date parsing
+  }
+
+  // If the current time is before the start time
+  if (isBefore(now, start)) {
+    return "Not started yet";
+  }
+  // If the auction is ongoing
+  else if (isAfter(now, start) && isBefore(now, end)) {
+    return "Ongoing";
+  }
+  // If the auction has ended
+  else {
+    const diffDays = differenceInDays(now, end);
 
     if (diffDays < 1) {
       return "Ended today";

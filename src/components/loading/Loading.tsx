280ed44db85cda78e0e66b5ec657895bfcb326a4
DuongNManh <manhduonglhp4@gmail.com>
2024-10-12 09:07:37 +0700

    fix: order and order detail component

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const Loading: React.FC = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <CircularProgress />
     </Box>
   );

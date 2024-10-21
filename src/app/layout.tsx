b51f1d9a52e947b31d9d74fec6cf1cf8cfb7ba11
lcaohoanq <hoangclw@gmail.com>
2024-10-21 16:31:20 +0700

    feat: add config nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import "../index.scss";
+
+export const metadata: Metadata = {
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  <html lang="en">
+    <body>
+      <div id="root">{children}</div>
+    </body>
+  </html>;
+}

24261c55e56f95f0d484aabd32adb08e3a87e0a2
dduongCongSon <156944788+dduongCongSon@users.noreply.github.com>
2024-11-22 10:51:09 +0700

    refactor: edit navigate

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,3 +2,4 @@ export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const passwordRegex =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 export const koiNameRegex = /^[A-Z][a-z]+(?: [A-Z][a-z]+)*$/;
+export const phoneRegex = /^(?:\+84|0084|0)[235789][0-9]{1,2}[0-9]{7}$/;
